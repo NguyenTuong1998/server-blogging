8e506d4255af54330af8ab8163044a8394380972
NguyenTuong <nhattuong.nguyen@kingfoodmart.net>
2024-08-03 00:10:28 +0700

    add feature comment

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import cloudinary from './configs/cloudinary.js'
 import upload from './middleware/multer.js'
 import Blog from './Schema/Blog.js'
 import Notification from './Schema/Notification.js'
+import Comment from './Schema/Comment.js'
 
 const server = express();
 let PORT = 3000;
@@ -390,6 +391,57 @@ server.post('/isLiked-by-user', verifyJWT, (req, res) => {
 
     Notification.exists({user: user_id, type: 'like', blog: _id})
     .then(result => res.status(200).json({result}))
+    .catch(err => res.status(500).json({error: err.message}))
+})
+
+server.post('/add-comment', verifyJWT, (req, res) => {
+    let user_id = req.user
+
+    let {_id, comment, replying_to, blog_author} = req.body
+
+    if(!comment.length) return res.status(403).json({error: 'Write something to leave a comment...'})
+
+    //create comment doc
+    let commentObj = new Comment({
+        blog_id: _id, blog_author, comment, commented_by: user_id
+    })
+
+    commentObj.save().then(commentFile => {
+        let { comment, commentedAt, children} = commentFile
+
+        Blog.findOneAndUpdate({_id}, {$push: {"comments": commentFile._id}, $inc: {"activity.total_comments": 1}, "activity.total_parent_comments" : 1})
+        .then(blog => {
+
+        })
+        
+        let noticationObj = {
+            type: 'comment',
+            blog: _id,
+            notification_for: blog_author,
+            user: user_id,
+            comment: commentFile._id
+        }
+
+        new Notification(noticationObj).save().then(notification => {})
+
+        return res.status(200).json({comment, commentedAt, _id: commentFile._id, children})
+    } )
+
+})
+
+server.post('/get-blog-comment', (req, res) => {
+    let {blog_id, skip} = req.body
+
+    let maxLimit = 5
+
+    Comment.find({blog_id, isReply: false})
+    .populate("comment_by", "personal_info.username", "personal_info.fullname", "personal_info.profile_img")
+    .skip(skip)
+    .limit(maxLimit)
+    .sort({
+        'commentedAt': -1 
+    })
+    .then(comment => res.status(200).json(comment))
     .catch(err => res.status(500).json({error: err.message}))
 })
 
